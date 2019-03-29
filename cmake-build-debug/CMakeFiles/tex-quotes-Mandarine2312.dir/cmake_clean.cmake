b2a4cca97190d24165d105c6f7ce81e929b00b28
Alejandro <alejandro.tang@utec.edu.pe>
2019-03-29 19:25:42 -0400

    Versión aceptada por tex

---
+++ cmake-build-debug/CMakeFiles/tex-quotes-Mandarine2312.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tex-quotes-Mandarine2312.dir/main.cpp.o"
+  "tex-quotes-Mandarine2312.pdb"
+  "tex-quotes-Mandarine2312"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tex-quotes-Mandarine2312.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -1,7 +1,54 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
-    return 0;
+
+
+int main() {
+  char c;
+  vector<char> lines;
+  bool state = true;
+  string line;
+
+  while(((scanf("%c", &c)) == 1)){
+      //lines.push_back(c);
+
+      if(c =='"'){
+          if(state){
+              cout << "``";
+              state = !state;
+          }else{
+              cout <<"\'\'";
+              state = !state;
+          }
+      }else{
+          cout << c;
+      }
+  }
+
+
+  for(auto l : lines) {
+    cout << l;
+  }
+
+  //asumiendo que se han guardado todas las líneas lines
+  for(int i = 0; i < lines.size(); i++){
+      if(lines[i] =='"'){
+        if(state){
+          lines[i] = '`';
+          lines.insert(lines.begin()+i, '`');
+        }else{
+          lines[i] = '\'';
+          lines.insert(lines.begin()+i, '\'');
+        }
+        state = !state;
+      }
+    }
+
+    for(auto l : lines) {
+        cout << l;
+    }
 }
+
